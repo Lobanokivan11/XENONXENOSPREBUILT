88487c5215bbd7412a20c3a8b28c7f3a7be35de7
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-03-21 16:31:01 +0300

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,6 @@
 wget -qO - https://apt.llvm.org/llvm.sh | sudo bash -s 20 all
 sudo apt update
-sudo apt-get install ninja-build cmake
+sudo apt-get install ninja-build cmake ld
 mkdir prebuilt-linux
 cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl-20 -DCMAKE_C_COMPILER=clang-cl-20 -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
