53acabaae6cfa0975c48e4d44687a6f7bfb71de2
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-03-21 16:34:31 +0300

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -6,11 +6,11 @@ cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl-20 -DCMAKE_C_COMPILER=clang-cl-20 -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
 cd build
 ninja
-cp XenonRecomp\XenonRecomp.exe ..\..\prebuilt-linux
-cp XenonAnalyse\XenonAnalyse.exe ..\..\prebuilt-linix
-cd ..\..\XenosRecomp
+cp XenonRecomp/XenonRecomp.exe ../../prebuilt-linux
+cp XenonAnalyse/XenonAnalyse.exe ../../prebuilt-linix
+cd ../../XenosRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl-20 -DCMAKE_C_COMPILER=clang-cl-20 -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
 cd build
 ninja
-cp XenosRecomp\XenosRecomp.exe ..\..\prebuilt-linux
-7z a -tzip ..\..\prebuilt-linux.zip ..\..\prebuilt-linux
+cp XenosRecomp/XenosRecomp.exe ../../prebuilt-linux
+7z a -tzip ../../prebuilt-linux.zip ../../prebuilt-linux
