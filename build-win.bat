603f11d6e4c42f779108861154878d5456108023
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-03-21 15:12:36 +0300

    Update build-win.bat

--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,6 @@
+choco install ninja -y
+choco install cmake -y
+choco install pkgconfiglite -y
 mkdir prebuilt-windows
 cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
