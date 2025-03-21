2f8eecf1819c3fdc2c2b9e661ab9ddd03efb0c2c
Lobanokivan11 <lobanokivan11@gmail.com>
2025-03-21 15:21:10 +0300

    F

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install clang ninja-build clang-cl
+sudo apt install ninja-build clang-19 clang-tools-19 clang-19-doc libclang-common-19-dev libclang-19-dev libclang1-19 clang-format-19 python3-clang-19 clangd-19 clang-tidy-19 libllvm-19-ocaml-dev libllvm19 llvm-19 llvm-19-dev llvm-19-doc llvm-19-examples llvm-19-runtime
 mkdir prebuilt-linux
 cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
--- build-win.bat
+++ build-win.bat
@@ -1,6 +1,7 @@
 choco install ninja -y
 choco install cmake -y
 choco install pkgconfiglite -y
+choco install llvm -y
 mkdir prebuilt-windows
 cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
