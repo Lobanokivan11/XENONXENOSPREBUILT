d7e16d20cf33255f921693562af7893efe28780b
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-03-21 15:10:38 +0300

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,4 @@
-sudo apt install clang pkgconfig
+sudo apt install clang pkgconfig ninja
 mkdir prebuilt-linux
 cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
