04052434862ebc488f784901963989138abd8090
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-03-21 15:45:39 +0300

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,5 @@
 sudo apt update
-sudo apt install ninja-build clang-19 clang-tools-19 clang-19-doc libclang-common-19-dev libclang-19-dev libclang1-19 clang-format-19 python3-clang-19 clangd-19 clang-tidy-19 libllvm-19-ocaml-dev libllvm19 llvm-19 llvm-19-dev llvm-19-doc llvm-19-examples llvm-19-runtime
-sudo update-alternatives --install --slave /usr/bin/clang-cl clang-cl /usr/bin/clang-cl-19
+apt-get install libllvm-21-ocaml-dev libllvm21 llvm-21 llvm-21-dev llvm-21-doc llvm-21-examples llvm-21-runtime clang-21 clang-tools-21 clang-21-doc libclang-common-21-dev libclang-21-dev libclang1-21 clang-format-21 python3-clang-21 clangd-21 clang-tidy-21 libclang-rt-21-dev libpolly-21-dev libfuzzer-21-dev lldb-21 lld-21 libc++-21-dev libc++abi-21-dev libomp-21-dev libclc-21-dev libunwind-21-dev libmlir-21-dev mlir-21-tools libbolt-21-dev bolt-21 flang-21 libclang-rt-21-dev-wasm32 libclang-rt-21-dev-wasm64 libc++-21-dev-wasm32 libc++abi-21-dev-wasm32 libclang-rt-21-dev-wasm32 libclang-rt-21-dev-wasm64 libllvmlibc-21-dev
 mkdir prebuilt-linux
 cd XenonRecomp
 cmake -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
